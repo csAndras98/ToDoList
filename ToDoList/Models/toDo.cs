d36fe85961ab7eacdb934cf2ee3ae08ec57f3717
tompuljalmeg <tompuljalmeg@gmail.com>
2021-04-21 01:59:24 +0200

    todo

---
+++ ToDoList/Models/toDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class toDo
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+        public bool Done { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
